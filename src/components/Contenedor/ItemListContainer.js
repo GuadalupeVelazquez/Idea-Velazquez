e53e3cfd65bb9da92cc47494109948160052c6ce
GuadalupeVelazquez <pupevelazquez6@gmail.com>
2022-08-02 16:19:01 -0300

    cambios en detail y list

--- src/components/Contenedor/ItemListContainer.js
+++ src/components/Contenedor/ItemListContainer.js
@@ -3,7 +3,7 @@ import {productos} from '../../mock/productos'
 import  ItemList  from './ItemList';
 import Loader from '../Loader/Loader'
 import { useParams } from 'react-router-dom';
-import {collection, getDocs, getFirestore} from 'firebase/firestore';
+import {collection, getDocs, getFirestore, query, where} from 'firebase/firestore';
 
 const styles = {
     style : {
@@ -18,21 +18,37 @@ const ItemListContainer = ({ textoSaludo }) => {
     const [carga, setCarga] = useState(true);
     const { categoria } = useParams()
 
-    const traerProductos = async () => {
-
-        const db = getFirestore();
-        await getDocs(collection(db, 'items'))
-            .then(snapshot => {
-                const dataExtraida = snapshot.docs.map((datos) => datos.data());
-                setItems(dataExtraida)
-                setCarga(false)
-            })
-    }
+    // const traerProductos = async () => {
 
 
-    useEffect(() => {
-        traerProductos()
-    }, [categoria]);
+        useEffect(() => {
+            const querydb = getFirestore ();
+            const queryCollection = collection (querydb, 'items');
+            if(categoria){
+            const queryFilter = query (queryCollection, where('categoria', '==', categoria))
+            getDocs (queryFilter)
+            .then (res => setItems(res.docs.map(item =>({id: item.id, ...item.data()}))))
+            } else {
+                getDocs (queryCollection)
+                .then (res => setItems(res.docs.map(item =>({id: item.id, ...item.data()}))))
+            }
+            setCarga(false)
+
+        }, [categoria ]);
+
+        // const db = getFirestore();
+        // await getDocs(collection(db, 'items'))
+        //     .then(snapshot => {
+        //         const dataExtraida = snapshot.docs.map((datos) => datos.data());
+        //         setItems(dataExtraida)
+        //         setCarga(false)
+        //     })
+    // }
+
+
+    // useEffect(() => {
+    //     traerProductos()
+    // }, [categoria]);
 
 
     return (<>{carga ? <Loader /> :
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -4,27 +4,46 @@ import {useState, useEffect} from 'react'
 import { productos } from '../../mock/productos'
 import Loader from '../Loader/Loader'
 import { useParams } from 'react-router-dom'
-import {collection, getDocs, getFirestore} from 'firebase/firestore';
+import {doc, getDoc, getFirestore} from 'firebase/firestore';
+
 
 const ItemDetailContainer = () => {
 const [item, setItem] = useState ({})
 const [carga, setCarga] = useState (true)
-const params=(useParams().id)-1
+const {params}=(useParams().id)-1
+// const {params}=useParams();
 
-const traerProductos = async () => {
+// const traerProductos = async () => {
 
-  const db = getFirestore();
-  await getDocs(collection(db, 'items'))
-      .then(snapshot => {
-          const dataExtraida = snapshot.docs.map((datos) => datos.data());
-          setItem(dataExtraida[params])
-          setCarga(false)
-      })
-}
+  useEffect(() => {
+    const querydb = getFirestore ();
+    const queryDoc = doc(querydb, 'items', '28fWOmBAB6zsHDy5EOE9'  ) ;
+    getDoc (queryDoc)
+    .then(res => setItem({id: res.id, ...res.data()})) 
+    setCarga(false)
+    
+   
+  }, []);
 
-useEffect(() => {
-  traerProductos()
-}, []);
+
+  // const db = getFirestore();
+  // await getDocs(collection(db, 'items'))
+  //     .then(snapshot => {
+  //         const dataExtraida = snapshot.docs.map((datos) => datos.data());
+  //         setItem(dataExtraida[params])
+  //         setCarga(false)
+  //     })
+
+
+  
+// }
+
+
+// useEffect(() => {
+
+//   traerProductos()
+// }, []);
+
 
   return (<>{carga ? <Loader/> :
     <ItemDetail item={item}/>
